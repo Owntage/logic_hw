dc3dff538a32c4bd2a426d4b34e372cfd83261f0
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-03-23 09:38:55 -0700

    generate arithmetic parse tree

--- source/axe_test.cpp
+++ source/axe_test.cpp
@@ -4,6 +4,7 @@
 
 #include <axe.h>
 #include <iostream>
+#include <stack>
 
 using namespace axe;
 
@@ -109,12 +110,134 @@ void check_expr(Rule rule, std::string input)
 	}
 }
 
+struct ExprTree
+{
+	std::string value;
+	ExprTree* left;
+	ExprTree* right;
+
+	ExprTree(const std::string& value) :
+			value(value),
+			left(nullptr),
+			right(nullptr)
+	{}
+
+	ExprTree(const std::string& value, ExprTree* left, ExprTree* right) :
+			value(value),
+			left(left),
+			right(right)
+	{}
+
+	template<typename Stream>
+	friend Stream& operator<<(Stream& s, ExprTree& tree)
+	{
+		s << tree.value;
+		if (tree.left != nullptr && tree.right != nullptr)
+		{
+			s << "(" << *tree.left << ", " << *tree.right << ")";
+		}
+		return s;
+	}
+};
+
+template<typename BaseRule, typename OpRule>
+ExprTree* generateBaseLevelTree(std::string input, BaseRule baseRule, OpRule operationRule)
+{
+	std::vector<std::string> operands;
+	std::vector<std::string> operations;
+	auto operandRule = baseRule >> e_push_back(operands);
+	auto operationExtractorRule = operationRule >> e_push_back(operations);
+	auto exprRule = r_many(operandRule, operationExtractorRule);
+	exprRule(input.begin(), input.end());
+	std::vector<ExprTree*> resultTrees;
+	for(int i = 0; i < operands.size(); i++)
+	{
+		resultTrees.push_back(new ExprTree(operands[i]));
+		if (i != operands.size() - 1)
+		{
+			resultTrees.push_back(new ExprTree(operations[i]));
+		}
+	}
+	if (resultTrees.size() > 1)
+	{
+		resultTrees[1]->left = resultTrees[0];
+		resultTrees[1]->right = resultTrees[2];
+	}
+	for (int i = 3; i < operands.size(); i+= 2)
+	{
+		resultTrees[i]->left = resultTrees[i-2];
+		resultTrees[i]->right = resultTrees[i+1];
+	}
+	if (resultTrees.size() > 1)
+	{
+		return resultTrees[resultTrees.size() - 2];
+	}
+	return resultTrees[0];
+};
+
+template<typename BaseRule, typename OpRule>
+ExprTree* generateTree(std::string input, BaseRule baseRule, std::vector<OpRule> operationRules)
+{
+	std::cout << "generating tree" << std::endl;
+	if (operationRules.size() == 0)
+	{
+		return new ExprTree(input);
+	}
+	if (operationRules.size() == 1)
+	{
+		return generateBaseLevelTree(input, baseRule, operationRules[0]);
+	}
+
+	r_rule<str_it> operand_rule;
+	operand_rule = r_many(baseRule, operationRules[0]);
+
+	for (int i = 1; i < operationRules.size() - 1; i++)
+	{
+		operand_rule = r_many(operand_rule, operationRules[i]);
+	}
+	std::vector<ExprTree*> operandTrees;
+	std::vector<OpRule> nextOperationRules = operationRules;
+	nextOperationRules.pop_back();
+
+
+	auto operand_tree_rule = *operand_rule >> e_ref([&operandTrees, &nextOperationRules, baseRule](str_it begin, str_it end)
+	{
+		std::string matched_str(begin, end);
+		operandTrees.push_back(generateTree(matched_str, baseRule, nextOperationRules));
+	});
+
+	std::vector<std::string> operators;
+
+	auto op_with_extract = operationRules.back() >> e_push_back(operators);
+	auto expr_rule = r_many(operand_tree_rule, op_with_extract);
+
+	expr_rule(input.begin(), input.end());
+
+	std::stack<ExprTree*> trees;
+	for (int i = 0; i < operandTrees.size(); i++)
+	{
+		trees.push(operandTrees[operandTrees.size() - 1 - i]);
+	}
+
+	int operatorIndex = 0;
+	while (trees.size() > 1)
+	{
+		ExprTree* first = trees.top();
+		trees.pop();
+		ExprTree* second = trees.top();
+		trees.pop();
+		ExprTree* newTree = new ExprTree(operators[operatorIndex++], first, second);
+		trees.push(newTree);
+	}
+	return trees.top();
+}
+
 void arithmetic_parser()
 {
 	auto variable = r_any("abc");
 	auto constant = r_numstr();
 	auto unary_expr = variable | constant;
-	auto mulOperations = r_char('*');
+	auto mulOperations = r_any("*/");
 	auto multiplication = r_many(unary_expr, mulOperations);
 	auto addOperations = r_any("+-");
 	auto addition = r_many(multiplication, addOperations);
@@ -128,6 +251,9 @@ void arithmetic_parser()
 	check_expr(expression, "a-b+c-b*255");
 	check_expr(expression, "");
 	check_expr(expression, "a*d");
+
+	ExprTree* exprTree = generateTree<decltype(unary_expr), decltype(mulOperations)>("a*b+a*b", unary_expr, {mulOperations, addOperations});
+	std::cout << "parsed expression: " << *exprTree << std::endl;
 }
 
 int main()
