95ff50ca61e9909d5ce22be90d0fc4244d763d66
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-03-24 00:41:47 -0700

    more axioms added

--- source/hw1.cpp
+++ source/hw1.cpp
@@ -28,6 +28,11 @@ bool notAnd(ExprTree* expr)
 	return expr->value != "&";
 }
 
+bool notOr(ExprTree* expr)
+{
+	return expr->value != "|";
+}
+
 bool notEq(ExprTree* first, ExprTree* second)
 {
 	return !(*first == *second);
@@ -59,19 +64,73 @@ bool checkAxiom2(ExprTree* impl1)
 
 bool checkAxiom3(ExprTree* impl1)
 {
-	
+	if (notImpl(impl1)) return false;
+	ExprTree* impl2 = impl1->right;
+	if (notImpl(impl2)) return false;
+	ExprTree* and3 = impl2->right;
+	if (notAnd(and3)) return false;
+
+	ExprTree* expr1 = impl1->left;
+	ExprTree* expr2 = impl2->left;
+
+	if (notEq(and3->left, expr1)) return false;
+	return !notEq(and3->right, expr2);
+}
+
+bool checkAxiom4(ExprTree* impl1)
+{
+	if (notImpl(impl1)) return false;
+	ExprTree* and1 = impl1->left;
+	if (notAnd(and1)) return false;
+
+	ExprTree* expr1 = and1->left;
+	return !notEq(impl1->right, expr1);
+}
+
+bool checkAxiom5(ExprTree* impl1)
+{
+	if (notImpl(impl1)) return false;
+	ExprTree* and1 = impl1->left;
+	if (notAnd(and1)) return false;
+
+	ExprTree* expr1 = and1->right;
+	return !notEq(impl1->right, expr1);
+}
+
+bool checkAxiom6(ExprTree* impl1)
+{
+	if (notImpl(impl1)) return false;
+	ExprTree* or1 = impl1->right;
+	if (notOr(or1)) return false;
+
+	ExprTree* expr1 = impl1->left;
+	return !notEq(or1->left, expr1);
+}
+
+bool checkAxioms()
+{
+	if (!checkAxiom1(PropositionalParser::parse("A->(B->A)"))) return false;
+	if (!checkAxiom2(PropositionalParser::parse("(A->B)->(A->B->C)->(A->C)"))) return false;
+	if (!checkAxiom3(PropositionalParser::parse("A->B->A&B"))) return false;
+	if (!checkAxiom4(PropositionalParser::parse("A&B->A"))) return false;
+	if (!checkAxiom5(PropositionalParser::parse("A&B->B"))) return false;
+	if (!checkAxiom6(PropositionalParser::parse("A->A|B"))) return false;
+	return true;
 }
 
 int main()
 {
-	std::cout << *PropositionalParser::parse("(A->B)->(A->B->C)->(A->C)") << std::endl;
-	if (checkAxiom2(PropositionalParser::parse("(A->B)->(A->B->C)->(A->C)")))
+	std::cout << "axiom3: " << *PropositionalParser::parse("A->B->A&B") << std::endl;
+	std::cout << "axiom4: " << *PropositionalParser::parse("A&B->A") << std::endl;
+	std::cout << "axiom5: " << *PropositionalParser::parse("A&B->B") << std::endl;
+	std::cout << "axiom6: " << *PropositionalParser::parse("A->A|B") << std::endl;
+	if (checkAxioms())
 	{
-		std::cout << "axiom1 true" << std::endl;
+		std::cout << "all axioms are tested" << std::endl;
 	}
 	else
 	{
-		std::cout << "axiom1 false" << std::endl;
+		std::cout << "failed to test axioms" << std::endl;
 	}
 	return 0;
 }
