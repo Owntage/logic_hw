3e3b1e3dee624b3c02b000b862df3faeb76ee9dd
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-03-24 04:20:18 -0700

    working with input file

--- source/hw1.cpp
+++ source/hw1.cpp
@@ -4,19 +4,61 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include "propositional_parser.h"
+#include <axe.h>
 
 using namespace std;
+using namespace axe;
 
+typedef std::string::iterator str_it;
+
+template<typename T>
+void printVector(std::vector<T> vector)
+{
+	for(int i = 0; i < vector.size(); i++)
+	{
+		std::cout << vector[i] << std::endl;
+	}
+}
 
 int main()
 {
-	std::cout << "axiom3: " << *PropositionalParser::parse("A->B->A&B") << std::endl;
-	std::cout << "axiom4: " << *PropositionalParser::parse("A&B->A") << std::endl;
-	std::cout << "axiom5: " << *PropositionalParser::parse("A&B->B") << std::endl;
-	std::cout << "axiom6: " << *PropositionalParser::parse("A->A|B") << std::endl;
-	std::cout << "axiom8: " << *PropositionalParser::parse("(A->C)->(B->C)->(A|B->C)") << std::endl;
-	std::cout << "axiom9: " << *PropositionalParser::parse("(A->B)->(A->!B)->!A") << std::endl;
-	std::cout << "axiom10: " << *PropositionalParser::parse("!!A->A") << std::endl;
+	std::cout << "enter filename: ";
+	std::string filename;
+	std::cin >> filename;
+	ifstream input_f(filename);
+
+	std::string input((std::istreambuf_iterator<char>(input_f)),
+					std::istreambuf_iterator<char>());
+
+	auto variable = r_any('A', 'Z') & *(r_any('A', 'Z') | r_num());
+	r_rule<str_it> unary_expr;
+	auto mulOperations = r_str("&");
+	auto addOperations = r_str("|");
+	auto implOperations = r_str("->");
+
+	auto multiplication = r_many(unary_expr, mulOperations);
+	auto addition = r_many(multiplication, addOperations);
+	auto implication = r_many(addition, implOperations);
+
+	unary_expr = *(r_char('!')) & variable | ("(" & implication & ")");
+
+	std::vector<string> assumptions;
+	std::vector<string> proofExpressions;
+
+	auto assumptionExpr = implication >> e_push_back(assumptions);
+	auto proofExpr = implication >> e_push_back(proofExpressions);
+	auto header = ~(assumptionExpr & *("," & assumptionExpr)) & r_str("|-") & implication;
+	auto proof = *(proofExpr & "\n");
+	auto inputGrammar = header & "\n" & proof;
+
+	inputGrammar(input.begin(), input.end());
+
+	std::cout << "extracted assumptions: " << std::endl;
+	printVector(assumptions);
+	std::cout << "extracted proof: " << std::endl;
+	printVector(proofExpressions);
+
 	return 0;
 }
