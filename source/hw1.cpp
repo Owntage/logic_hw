b056963d37f89edc6edfb4a4e5faa246067be968
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-03-23 18:15:03 -0700

    axiom check example

--- source/hw1.cpp
+++ source/hw1.cpp
@@ -3,13 +3,37 @@
 //
 
 #include <iostream>
+#include <vector>
 #include "propositional_parser.h"
 
 using namespace std;
 
+vector<ExprTree*> assumptions;
+
+bool checkAxiom1(ExprTree* expr)
+{
+	if (expr->value != "->") return false;
+	if (expr->left == nullptr || expr->right == nullptr) return false;
+	ExprTree* rightExpr = expr->right;
+	if (rightExpr->value != "->") return false;
+	if (rightExpr->left == nullptr || rightExpr->right == nullptr) return false;
+	return *expr->left == *rightExpr->right;
+}
+
+bool checkAxiom2(ExprTree* expr)
+{
+	
+}
+
 int main()
 {
-	ExprTree* result = PropositionalParser::parse("(a)+(b)");
-	cout << "parsed expression: " << *result << ". " << endl;
+	if (checkAxiom1(PropositionalParser::parse("(A->B)->(B->(A->B))")))
+	{
+		std::cout << "axiom1 true" << std::endl;
+	}
+	else
+	{
+		std::cout << "axiom1 false" << std::endl;
+	}
 	return 0;
 }
