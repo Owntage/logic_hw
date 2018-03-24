3177af7ded4926ca84d63ce74fc0cfdf1ec693c9
Igor Ilyin <Igor.Ilyin@jetbrains.com>
2018-03-23 18:58:55 -0700

    axiom2

--- source/hw1.cpp
+++ source/hw1.cpp
@@ -13,21 +13,49 @@ vector<ExprTree*> assumptions;
 bool checkAxiom1(ExprTree* expr)
 {
 	if (expr->value != "->") return false;
-	if (expr->left == nullptr || expr->right == nullptr) return false;
 	ExprTree* rightExpr = expr->right;
 	if (rightExpr->value != "->") return false;
-	if (rightExpr->left == nullptr || rightExpr->right == nullptr) return false;
 	return *expr->left == *rightExpr->right;
 }
 
-bool checkAxiom2(ExprTree* expr)
+bool notImpl(ExprTree* expr)
 {
-	
+	return expr->value != "->";
+}
+
+bool notEq(ExprTree* first, ExprTree* second)
+{
+	return !(*first == *second);
+}
+
+bool checkAxiom2(ExprTree* impl1)
+{
+	if (notImpl(impl1)) return false;
+	ExprTree* impl2 = impl1->left;
+	if (notImpl(impl2)) return false;
+	ExprTree* impl3 = impl1->right;
+	if (notImpl(impl3)) return false;
+	ExprTree* impl4 = impl3->left;
+	if (notImpl(impl4)) return false;
+	ExprTree* impl5 = impl3->right;
+	if (notImpl(impl5)) return false;
+	ExprTree* impl6 = impl4->right;
+	if (notImpl(impl6)) return false;
+
+	ExprTree* expr1 = impl2->left;
+	ExprTree* expr2 = impl2->right;
+	ExprTree* expr3 = impl6->right;
+
+	if (notEq(impl4->left, expr1)) return false;
+	if (notEq(impl6->left, expr2)) return false;
+	if (notEq(impl5->left, expr1)) return false;
+	return !notEq(impl5->right, expr3);
 }
 
 int main()
 {
-	if (checkAxiom1(PropositionalParser::parse("(A->B)->(B->(A->B))")))
+	std::cout << *PropositionalParser::parse("(A->B)->(A->B->C)->(A->C)") << std::endl;
+	if (checkAxiom2(PropositionalParser::parse("(A->B)->(A->B->C)->(A->C)")))
 	{
 		std::cout << "axiom1 true" << std::endl;
 	}
